bd61328bddf7e1093834f88f5c24638334130743
AmmrYsir <ammaryasir03062000@gmail.com>
2026-07-20 10:09:44 +0800

    Set default width and height attributes for icons in Blade configuration

--- config/blade-icons.php
+++ config/blade-icons.php
@@ -93,8 +93,8 @@ return [
             */
         
             'attributes' => [
-                // 'width' => 50,
-                // 'height' => 50,
+                'width' => 50,
+                'height' => 50,
             ],
         
         ],
