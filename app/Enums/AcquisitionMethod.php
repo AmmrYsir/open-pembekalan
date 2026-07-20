d9edce73e550a16df8dc0364dc998d8547be3505
AmmrYsir <ammaryasir03062000@gmail.com>
2026-07-20 15:20:32 +0800

    Add label method to AcquisitionMethod enum for better readability

--- app/Enums/AcquisitionMethod.php
+++ app/Enums/AcquisitionMethod.php
@@ -8,6 +8,15 @@ enum AcquisitionMethod: string
     case PERKHIDMATAN = 'PERKHIDMATAN';
     case BEKALAN_DAN_PERKHIDMATAN = 'BEKALAN DAN PERKHIDMATAN';
 
+	public function label(): string
+    {
+        return match ($this) {
+            self::BEKALAN => 'Bekalan',
+            self::PERKHIDMATAN => 'Perkhidmatan',
+            self::BEKALAN_DAN_PERKHIDMATAN => 'Bekalan dan Perkhidmatan',
+        };
+    }
+
     /**
      * @return array<int, string>
      */
